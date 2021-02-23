4ff6c6ea22090af1f2df3141019ff8f81c4c2514
Raghad <78907185+Raghad400@users.noreply.github.com>
2021-02-23 11:22:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare variables and then initialize to zero.
+            double num1 = 0;
+            double num2 = 0;
+
+            bool check = false;
+            while (!check)
+            {
+
+                // Ask the user to type the first number.
+                Console.WriteLine("Type a number, and then press Enter");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                // Ask the user to type the second number.
+                Console.WriteLine("Type another number, and then press Enter");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                // Ask the user to choose an option.
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\t+ - Add");
+                Console.WriteLine("\t- - Subtract");
+                Console.WriteLine("\t* - Multiply");
+                Console.WriteLine("\t/ - Divide");
+                Console.Write("Your option? ");
+
+                // Use a switch statement to do the math.
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "-":
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "*":
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "/":
+
+                        while (num2 == 0)
+                        {
+                            Console.WriteLine("Type a non-zero divisor: ");
+                            num2 = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") check = true;
+
+                Console.WriteLine("\n"); 
+            }
+            return;
+
+        }
+    }
+}
+
